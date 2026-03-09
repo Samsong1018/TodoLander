8116861bbd7e88267563f5456b4ef06af910c981
Amos Horne <amoshorne@gmail.com>
2026-03-09 00:01:12 -0700

    small update

--- api/server.js
+++ api/server.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const uuid = require('uuid')
+
 const app = express();
 const PORT = 3000;
+
+/* Singup and Login endpoints */
 
 app.post('/api/signup', (req, res) => {
     console.log("signup");
 });
 
 app.post('/api/login', (req, res) => {
-    console.log("Login");
+    const { username, password } = req.body;
+    const token = uuid.v4();
+
+    /* check if users in database */
+
+    
 });
 
 app.listen(PORT, () => {
